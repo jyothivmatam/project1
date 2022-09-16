950810753bd2f037077b252a5ffc3728f6e1afca
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-09-16 11:08:41 +0530

    Add files via upload

---
+++ TestExample2/TestExample2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+public class Exercise1
+{
+    public static void Main()
+    {
+        char letter, letter1, letter2;
+        Console.Write("Input leter: ");
+        letter = Convert.ToChar(Console.ReadLine());
+        Console.Write("Input leter: ");
+        letter1 = Convert.ToChar(Console.ReadLine());
+        Console.Write("Input leter: ");
+        letter2 = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine("{0} {1} {2}", letter2, letter1, letter);
+
+    }
+}
