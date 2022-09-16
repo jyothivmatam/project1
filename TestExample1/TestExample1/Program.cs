48ebc58901ad2106e5816a5c551396c638a9414c
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-09-16 11:05:21 +0530

    Add files via upload

---
+++ TestExample1/TestExample1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class Exercise1
+{
+    public static void Main()
+    {
+        int i;
+        Console.WriteLine("The First 10 natural numbers are:");
+        for(i=0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
